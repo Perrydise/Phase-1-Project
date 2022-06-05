d5fe86c5ba5b80e8d7c75fe3db1de63e0bfe6dff
Perry Jorss <pjorss123@gmail.com>
2022-06-05 19:03:08 -0400

    prettyfied the code

--- Phase-1 Project/index.js
+++ Phase-1 Project/index.js
@@ -1,37 +1,39 @@
 let addToy = false;
-//This function waits for the dom content to be loaded and then allows the user to access the buttons
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   const submitForm = document.getElementById("submitForm");
   submitForm.addEventListener("submit", cardSubmitter);
   addBtn.addEventListener("click", () => {
-    showFormHelper(toyFormContainer)
+    showFormHelper(toyFormContainer);
   });
-})
-//cardSubmitter verifies the users drink and allows the user to input data into the drink fields
+});
+
 function cardSubmitter(event) {
-    event.preventDefault();
-    const submitForm = document.getElementById("submitForm");
-    const drinkName = submitForm.querySelector('input[name="name"]').value;
-    const drinkImg = submitForm.querySelector('input[name="image"]').value;
-        if(drinkName === "" || drinkImg === ""){
-        alert("You are missing a required field!")
-        } else {
-        fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName)
+  event.preventDefault();
+  const submitForm = document.getElementById("submitForm");
+  const drinkName = submitForm.querySelector('input[name="name"]').value;
+  const drinkImg = submitForm.querySelector('input[name="image"]').value;
+  if (drinkName === "" || drinkImg === "") {
+    alert("You are missing a required field!");
+  } else {
+    fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName
+    )
       .then((resp) => {
         return resp.json();
       })
       .then((data) => {
         findAndCreateDrinkCard(data.drinks, drinkName, drinkImg);
       });
-      }
   }
-// cardCreator creates a element to push to the dom to display the card of the users inputted drink
+}
+
 /**
- * 
- * @param {Object} drink 
- * @param {*Object} verifiedDrinkObject 
+ *
+ * @param {Object} drink
+ * @param {*Object} verifiedDrinkObject
  * @returns HTML element with drink card info
  */
 function cardCreator(drink, verifiedDrinkObject) {
@@ -40,17 +42,17 @@ function cardCreator(drink, verifiedDrinkObject) {
   const cardHeader = document.createElement("h2");
   const verifiedCard = document.createElement("h3");
   const cardImg = document.createElement("img");
-  cardImg.classList.add("img-center")
-  cardImg.width = "200"
-  cardImg.height = "150"
+  cardImg.classList.add("img-center");
+  cardImg.width = "200";
+  cardImg.height = "150";
   const cardParagraph = document.createElement("p");
   const cardLike = document.createElement("button");
   cardLike.addEventListener("click", () => {
-    buttonHelper("Like", cardParagraph, cardDislike, cardLike)
+    buttonHelper("Like", cardParagraph, cardDislike, cardLike);
   });
   const cardDislike = document.createElement("button");
   cardDislike.addEventListener("click", () => {
-    buttonHelper("Dislike", cardParagraph, cardDislike, cardLike)
+    buttonHelper("Dislike", cardParagraph, cardDislike, cardLike);
   });
   cardHeader.innerText = drink.drinkName;
   cardImg.src = drink.drinkImg;
@@ -61,8 +63,8 @@ function cardCreator(drink, verifiedDrinkObject) {
     verifiedCard.innerText = "Verified";
     cardDiv.appendChild(verifiedCard);
   }
-  if (verifiedDrinkObject && verifiedDrinkObject.strDrinkThumb){
-      cardImg.src = verifiedDrinkObject.strDrinkThumb
+  if (verifiedDrinkObject && verifiedDrinkObject.strDrinkThumb) {
+    cardImg.src = verifiedDrinkObject.strDrinkThumb;
   }
   cardDiv.appendChild(cardImg);
   cardDiv.appendChild(cardParagraph);
@@ -70,7 +72,7 @@ function cardCreator(drink, verifiedDrinkObject) {
   cardDiv.appendChild(cardDislike);
   return cardDiv;
 }
-//findAndCreateDrinkCard allows the user to check if their drink is verified or not
+
 function findAndCreateDrinkCard(drinks, drinkName, drinkImg) {
   let verified;
   if (drinks) {
@@ -84,23 +86,23 @@ function findAndCreateDrinkCard(drinks, drinkName, drinkImg) {
   container.appendChild(htmlDrink);
 }
 
-function showFormHelper(toyFormContainer){
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  };
+function showFormHelper(toyFormContainer) {
+  addToy = !addToy;
+  if (addToy) {
+    toyFormContainer.style.display = "block";
+  } else {
+    toyFormContainer.style.display = "none";
+  }
+}
 
-  function buttonHelper(action, cardParagraph, cardDislike, cardLike){
-    if(action === "Like"){
+function buttonHelper(action, cardParagraph, cardDislike, cardLike) {
+  if (action === "Like") {
     cardParagraph.innerText = "You like this drink!";
     cardLike.style.visibility = "hidden";
     cardDislike.style.visibility = "visible";
-    } else {
+  } else {
     cardParagraph.innerText = "You don't like this drink!";
     cardDislike.style.visibility = "hidden";
-    cardLike.style.visibility = "visible"; 
-    }
+    cardLike.style.visibility = "visible";
   }
+}
